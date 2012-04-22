e065c8dc77fe71cd225d7ea569401e89abca4ff2
Dakota <dstrickner@gmail.com>
2012-04-21 22:37:14 -0400

    added beginings of possible custom death message.. also added creeper spawn at explosion location only for cross hair smite

--- Smite.java
+++ Smite.java
@@ -15,6 +15,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Smite extends JavaPlugin {
    public static Smite plugin;
    public final Logger logger = Logger.getLogger("Minecraft");
+   public String deathMessage = "Boom";
  
    public void onDisable() {
 	  PluginDescriptionFile pdffile = this.getDescription();
@@ -24,11 +25,14 @@ public class Smite extends JavaPlugin {
    public void onEnable() {
 	  PluginDescriptionFile pdffile = this.getDescription();
 	  this.logger.info(pdffile.getName() + "is now enabled.");
+	  getConfig().options().copyDefaults(true);
+	  this.saveDefaultConfig();
    }
 
    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 	   Player player = (Player) sender;
 	   World world = player.getWorld();
+	   this.deathMessage = "boom";
 	   if(commandLabel.equalsIgnoreCase("smite")) {
 		   if(args.length == 0) {
 			   Block targetblock = player.getTargetBlock(null,  50);
@@ -37,11 +41,18 @@ public class Smite extends JavaPlugin {
 			   //get "blast-radius" config, defualt to 10 if none set. Thanks to morganm for the help :) no good deed goes un-noticed
 			   int radius = getConfig().getInt("blast-radius", 10);
 			   world.createExplosion(location, radius);
+			   //To spawn a creeper no idea how this will work.. Will eventually include a var in the config.
+			   world.spawnCreature(location, org.bukkit.entity.EntityType.CREEPER);
+			   world.canGenerateStructures();
+			   //next line is for the player variable. /smite [playername] 
 		   } else if (args.length == 1) {
 			   if(player.getServer().getPlayer(args[0]) != null) {
 				   Player targetplayer = player.getServer().getPlayer(args[0]);
 				   Location location = targetplayer.getLocation();
 				   world.strikeLightning(location);
+				   //get "blast-player" config, defualt to 0 if none set, set 0 to create no explosion. Thanks to morganm for the help :) no good deed goes un-noticed
+				   int radius = getConfig().getInt("blast-player", 0);
+				   world.createExplosion(location, radius);
 				   player.sendMessage(ChatColor.GRAY + "Smiting Player " + targetplayer.getDisplayName());
 			   } else {
 				   player.sendMessage(ChatColor.RED + "Error: The player is offline. ");
