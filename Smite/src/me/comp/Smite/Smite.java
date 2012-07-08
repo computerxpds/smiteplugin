35d95f657ecac7df2a0221095b5fa5560e58c27e
Dakota <dstrickner@gmail.com>
2012-07-08 00:32:42 -0400

    added (hopefuly) broadcast message on strike player

--- Smite/src/me/comp/Smite/Smite.java
+++ Smite/src/me/comp/Smite/Smite.java
@@ -14,7 +14,8 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Smite extends JavaPlugin {
-   public static Smite plugin;
+   private static final byte[] Death = null;
+public static Smite plugin;
    public final Logger logger = Logger.getLogger("Minecraft");
    public String deathMessage = "Boom";
  
@@ -44,7 +45,7 @@ public class Smite extends JavaPlugin {
 			   world.createExplosion(location, radius);
 			   //To spawn a creeper no idea how this will work.. Will eventually include a var in the config.
 			   world.spawnCreature(location, org.bukkit.entity.EntityType.CREEPER);
-			   world.canGenerateStructures();
+			   world.spawnCreature(location, org.bukkit.entity.EntityType.PIG_ZOMBIE);
 			   //next line is for the player variable. /smite [playername] 
 		   } else if (args.length == 1) {
 			   if(player.getServer().getPlayer(args[0]) != null) {
@@ -58,6 +59,7 @@ public class Smite extends JavaPlugin {
 				   world.spawnCreature(location, org.bukkit.entity.EntityType.PIG_ZOMBIE);
 				   //Shows spawner effect ~Comp
 				   world.playEffect(location, Effect.MOBSPAWNER_FLAMES, 0);
+				   world.sendPluginMessage(plugin, deathMessage, Death);
 				   player.sendMessage(ChatColor.GRAY + "Smiting Player " + targetplayer.getDisplayName());
 			   } else {
 				   player.sendMessage(ChatColor.RED + "Error: The player is offline. ");
