f3325733a41c857578727d797ed267d34d22a867
Dakota <dstrickner@gmail.com>
2012-08-23 19:09:32 -0400

    updated to 1.3.1 r2.0 cb

--- src/me/comp/Smite/Smite.java
+++ src/me/comp/Smite/Smite.java
@@ -36,6 +36,8 @@ public class Smite extends JavaPlugin {
 	   this.deathMessage = "boom";
 	   if(commandLabel.equalsIgnoreCase("smite")) {
 		   if(args.length == 0) {
+			   //permissions line, op only for now. ~Comp added v. 3.6P
+			   if(player.hasPermission("smite.ground")) {
 			   Block targetblock = player.getTargetBlock(null,  50);
 			   Location location = targetblock.getLocation();
 			   world.strikeLightning(location);
@@ -43,27 +45,55 @@ public class Smite extends JavaPlugin {
 			   int radius = getConfig().getInt("blast-radius", 10);
 			   world.createExplosion(location, radius);
 			   //To spawn a creeper no idea how this will work.. Will eventually include a var in the config.
-			   world.spawnCreature(location, org.bukkit.entity.EntityType.CREEPER);
-			   world.canGenerateStructures();
-			   //next line is for the player variable. /smite [playername] 
+			   //world.spawnCreature(location, org.bukkit.entity.EntityType.CREEPER);
+			   world.spawnEntity(location, org.bukkit.entity.EntityType.CREEPER);
+			   world.spawnEntity(location, org.bukkit.entity.EntityType.PIG_ZOMBIE);
+			   //world.spawnCreature(location, org.bukkit.entity.EntityType.PIG_ZOMBIE);
+			   world.createExplosion(location, radius, isEnabled());
+			   }else{
+				   player.sendMessage("You do not have permission to do this");
+			   }
+			   //next line is for the player variable. /smite [playername] ~added in V1.0
 		   } else if (args.length == 1) {
 			   if(player.getServer().getPlayer(args[0]) != null) {
 				   Player targetplayer = player.getServer().getPlayer(args[0]);
 				   Location location = targetplayer.getLocation();
+				   if(player.hasPermission("smite.player")) {
 				   world.strikeLightning(location);
 				   //get "blast-player" config, defualt to 0 if none set, set 0 to create no explosion. Thanks to morganm for the help :) no good deed goes un-noticed
 				   int radius = getConfig().getInt("blast-player", 0);
 				   world.createExplosion(location, radius);
-				   //Spawns pig zombie ~Comp
-				   world.spawnCreature(location, org.bukkit.entity.EntityType.PIG_ZOMBIE);
-				   //Shows spawner effect ~Comp
+				   world.createExplosion(location, radius, isEnabled());
+				   //Spawns pig zombie ~Comp ~Added in V3.0
+				   world.spawnEntity(location, org.bukkit.entity.EntityType.PIG_ZOMBIE);
+				   //world.spawnCreature(location, org.bukkit.entity.EntityType.PIG_ZOMBIE);
+				   //Shows spawner effect ~Comp ~Added in V3.0
 				   world.playEffect(location, Effect.MOBSPAWNER_FLAMES, 0);
 				   player.sendMessage(ChatColor.GRAY + "Smiting Player " + targetplayer.getDisplayName());
+				   //Should add in name on smite to broadcast to server~Comp V3.2 V3.3 added space
+				   getServer().broadcastMessage(ChatColor.RED + "Smiting Player " + targetplayer.getDisplayName());
+				   //Permission denied line. ~Comp
+				   //New feature in the works.. obv isnt finished yet but when it is this will ensure the player DIES when run.~Comp
+		   } else if (args.length == 2) {
+			   if(commandLabel.equalsIgnoreCase("smitekill")) {
+			   if(player.getServer().getPlayer(args[0]) != null) {
+				   Location locationthis = targetplayer.getLocation();
+				   if(player.hasPermission("smite.playerkill")) {
+					   world.createExplosion(locationthis, 1);
+					   world.setStorm(isEnabled());
+					   getServer().broadcastMessage(ChatColor.RED + "Killing This Player With DeathSmite: " + targetplayer.getDisplayName());
+				   }
+				   }
+						   
+					   }
+				   }else{
+					   player.sendMessage(ChatColor.RED + "You do not have permission to do this");
+				   }
 			   } else {
-				   player.sendMessage(ChatColor.RED + "Error: The player is offline. ");
+				   player.sendMessage(ChatColor.RED + "Error: The player is offline please use a different player. ");
 			   }
 		   } else if (args.length > 1) {
-			   player.sendMessage(ChatColor.RED + "Error: Too Many Arguments!");
+			   player.sendMessage(ChatColor.RED + "Error: Too Many Args!");
 		   }
 	   }
 	   return false;
