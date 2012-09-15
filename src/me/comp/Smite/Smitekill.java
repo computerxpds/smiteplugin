b296e42ca46b1227107fdb4b8ef6f129a7e7cd9d
Dakota <dstrickner@gmail.com>
2012-09-15 12:56:11 -0400

    trying again. command works but doesnt.. confusing eh?

--- src/me/comp/Smite/Smitekill.java
+++ src/me/comp/Smite/Smitekill.java
@@ -7,10 +7,12 @@ import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.logging.Logger;
 
-public class Smitekill extends Smite {
-	   public static Smite plugin;
+public class Smitekill extends JavaPlugin {
+	   public static Smitekill plugin;
 	   public final Logger logger = Logger.getLogger("Minecraft");
 	   
 public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
@@ -19,25 +21,23 @@ public boolean onCommand(CommandSender sender, Command cmd, String commandLabel,
 	   if(commandLabel.equalsIgnoreCase("smitekill")) {
 		   if(args.length == 0) {
 			   //permissions line, op only for now. ~Comp added v. 3.6P
-			   if(player.hasPermission("smite.ground")) {
 			   Block targetblock = player.getTargetBlock(null,  50);
 			   Location location = targetblock.getLocation();
 			   world.strikeLightning(location);
 			   //get "blast-radius" config, defualt to 10 if none set. Thanks to morganm for the help :) no good deed goes un-noticed
-			   int radius = getConfig().getInt("blast-radius", 10);
-			   world.createExplosion(location, radius);
+			   //int radius = getConfig().getInt("blast-radius", 10);
+			   world.createExplosion(location, 5);
 			   //To spawn a creeper no idea how this will work.. Will eventually include a var in the config.
 			   //world.spawnCreature(location, org.bukkit.entity.EntityType.CREEPER);
 			   world.spawnEntity(location, org.bukkit.entity.EntityType.CREEPER);
 			   world.spawnEntity(location, org.bukkit.entity.EntityType.PIG_ZOMBIE);
 			   //world.spawnCreature(location, org.bukkit.entity.EntityType.PIG_ZOMBIE);
-			   world.createExplosion(location, radius, isEnabled());
+			   world.createExplosion(location, 5, isEnabled());
 			   }else{
 				   player.sendMessage("You do not have permission to do this");
 				   
 			   }
 		   }
-	   }
 	return false;
 }
 }
